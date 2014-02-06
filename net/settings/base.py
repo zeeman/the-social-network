0ea5cf3b9f698adcf21f9c5ef2d102eb17c84d25
drkitty <bumblingkitty@gmail.com>
2014-02-06 10:01:34 -0800

    Split settings into base and local

--- net/settings/base.py
+++ net/settings/base.py
@@ -16,17 +16,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'c2)@d41w=-hsmz5r+b8(-2$y5&$1k#hueq#98l@p=10q#)v*u#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = (
@@ -51,30 +40,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'net.urls'
 
 WSGI_APPLICATION = 'net.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
