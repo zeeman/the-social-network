309e851d104878a497d16a8db9b229585411c969
drkitty <bumblingkitty@gmail.com>
2014-02-07 22:19:38 -0800

    Do some JS magic

--- static/js/blogfrog.js
+++ static/js/blogfrog.js
@@ -1,7 +1,13 @@
-$('.message .close').on('click', function() {
-  $(this).closest('.message').fadeOut();
-});
+$(document).ready(function() {
+    $('.message .close').on('click', function() {
+        $(this).closest('.message').fadeOut();
+    });
 
-$('.ui.popup').popup({
-    on: 'click'
+    $('.ui.popup').popup({
+        on: 'click'
+    });
+
+    $('#edit_profile').on('click', function() {
+        document.location.href = '/edit_profile';
+    });
 });
