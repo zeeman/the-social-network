da6c001643061a003621177240dd8bd997daf47a
Zane Salvatore <zeeman13@gmail.com>
2014-02-07 17:50:51 -0800

    Add net/settings/local.py to .gitignore

---
+++ net/friends/api/v1/api.py
@@ -0,0 +1,54 @@
+from rest_framework import serializers, viewsets
+from net.friends import models as net_models
+
+
+class RelationshipSerializer(serializers.ModelSerializer):
+    from_user = serializers.HyperlinkedRelatedField(many=False, read_only=True,
+                                                    view_name="user-detail")
+    to_user = serializers.HyperlinkedRelatedField(many=False, read_only=True,
+                                                  view_name="user-detail")
+
+    class Meta:
+        model = net_models.Relationship
+
+
+class SubscriptionSerializer(RelationshipSerializer):
+    class Meta:
+        model = net_models.Relationship
+        fields = ['to_user']
+
+
+class SubscriberSerializer(RelationshipSerializer):
+    class Meta:
+        model = net_models.Relationship
+        fields = ['from_user']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    user = serializers.HyperlinkedRelatedField(many=False, read_only=True,
+                                               view_name="user-detail")
+
+    class Meta:
+        model = net_models.Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    model = net_models.Post
+    serializer_class = PostSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    subscriptions = SubscriptionSerializer(many=True)
+    subscribers = SubscriberSerializer(many=True)
+    posts = serializers.HyperlinkedRelatedField(many=True, read_only=True,
+                                                view_name='post-detail')
+
+    class Meta:
+        model = net_models.User
+        fields = ['name', 'about', 'join_date', 'subscriptions', 'subscribers',
+                  'posts']
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    model = net_models.User
+    serializer_class = UserSerializer
--- net/urls.py
+++ net/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from net.friends.api.v1.routes import router
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
+    url(r'api/v1/', include(router.urls)),
 )
