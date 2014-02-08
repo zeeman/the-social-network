c9459b4d76f732d41d2c79916645609a4fedccd1
drkitty <bumblingkitty@gmail.com>
2014-02-07 23:09:33 -0800

    Remove old button listener

--- static/js/blogfrog.js
+++ static/js/blogfrog.js
@@ -6,8 +6,4 @@ $(document).ready(function() {
     $('.ui.popup').popup({
         on: 'click'
     });
-
-    $('#edit_profile').on('click', function() {
-        document.location.href = '/edit_profile';
-    });
 });
