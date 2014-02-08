6aae61c4cf5cce46e740d051deec8dabed274443
drkitty <bumblingkitty@gmail.com>
2014-02-07 16:57:18 -0800

    Serve static content

--- net/settings/base.py
+++ net/settings/base.py
@@ -50,3 +50,7 @@ WSGI_APPLICATION = 'net.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
