d16542da6a9f0425b735642cba622e5598f79ee9
drkitty <bumblingkitty@gmail.com>
2014-02-07 17:06:46 -0800

    Add some bare-bones views

--- net/urls.py
+++ net/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from net.views import basic_view
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +12,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^account/', basic_view('account')),
+    url(r'^feed/', basic_view('feed')),
+    url(r'^profile/', basic_view('profile')),
 )
